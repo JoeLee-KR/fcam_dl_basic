7d6627293f8c0b1eb536d4984a273443b60ee489
Joe Lee <xjoelee@gmail.com>
2022-07-26 15:11:33 +0900

    DoogieJoe:220726-1511

--- myEx/00_selfTest/00_testPy.py
+++ myEx/00_selfTest/00_testPy.py
@@ -1,17 +1,18 @@
 # bellow for python3.7,
 # not need at python3.8
-#import os
-#os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+# import os
+# os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 import numpy as np
-print('numpy np',np.__version__)
+print('>numpy np:',np.__version__)
 
 import scipy as sp
-print('scipy sp',sp.__version__)
+print('>scipy sp:',sp.__version__)
 
-import torch as tc
-print('torch tc',tc.__version__)
+import torch as tp
+tp.__version__
+print(">pyTorch tp:", tp.__version__)
 
 import tensorflow as tf
-print("Tensorflow:", tf.__version__)
+print(">Tensorflow:", tf.__version__)
 

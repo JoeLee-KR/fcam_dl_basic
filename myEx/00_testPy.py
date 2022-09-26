e16eaf5ebb5e527781fb08c988b940c2e192ba30
Joe Lee <xjoelee@gmail.com>
2022-09-26 11:24:44 +0900

    DoogieJoe:220926-1124

---
+++ myEx/00_testPy.py
@@ -0,0 +1,17 @@
+# bellow for python3.7,
+# not need at python3.8
+#import os
+#os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+import numpy as np
+print('numpy np',np.__version__)
+
+import scipy as sp
+print('scipy sp',sp.__version__)
+
+import torch as tc
+print('torch tc',tc.__version__)
+
+import tensorflow as tf
+print("Tensorflow:", tf.__version__)
+

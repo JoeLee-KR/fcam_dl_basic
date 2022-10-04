2e856dabc459c106538f6ba009e6c853891ea2ce
NYX <jungmin-kr@icloud.com>
2022-10-04 13:51:51 +0900

    nyxMac:221004-1351

--- myEx/00_selfTest/00_testPy.py
+++ myEx/00_selfTest/00_testPy.py
@@ -1,19 +1,17 @@
 # bellow for python3.7,
-# not need over python3.8
-import os
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+# not need at python3.8
+#import os
+#os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 import numpy as np
-print('>numpy np:',np.__version__)
+print('numpy np',np.__version__)
 
 import scipy as sp
-print('>scipy sp:',sp.__version__)
+print('scipy sp',sp.__version__)
 
-import torch as tp
-tp.__version__
-print(">pyTorch tp:", tp.__version__)
+import torch as tc
+print('torch tc',tc.__version__)
 
 import tensorflow as tf
-print(">Tensorflow:", tf.__version__)
+print("Tensorflow:", tf.__version__)
 
-print("Test End. by PY")
